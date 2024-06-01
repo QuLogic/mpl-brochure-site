c17e43bbab674e0ad72293241869e60766216b22
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2024-06-01 02:29:31 -0400

    Convert HTML site to reST

--- docs/_static/images-rotate/_generate_images.py
+++ docs/_static/images-rotate/_generate_images.py
@@ -1,6 +1,7 @@
 
+import json
+import logging
 from pathlib import Path
-import logging 
 
 import matplotlib
 matplotlib.use('Agg')
@@ -8,28 +9,34 @@ matplotlib.use('Agg')
 _log = logging.getLogger(__name__)
 # start to make info.js
 
-stout = 'var images_rotate = [\n'
-# assumes that your directory structure has matplotlib/ and mpl-brochure-site/ 
-# at the same level relative each other.  
-home = Path('../../../../matplotlib/')
-dirs = ['plot_types/arrays/', 'plot_types/basic/', 'plot_types/stats/']
-for d in dirs:
-    _log.info('working on: %s', d)
-    for fn in (home / d).glob('*.py'):
-        _log.info('               %s', fn)
-        name = fn.stem
-        exec(fn.read_text())
-        fig.savefig(f'{name}300.png', dpi=300)
-        # fig.savefig(f'{name}150.png', dpi=150)
-        stop = False
-        for line in open(fn):
-            if stop:
-                cap = line[:-1]
-                break
-            if line[:3] == '===':
-                stop = True            
-        stout += '    {' + f'"image": "{name}300.png", "caption": "{cap}", "link": "{d}{name}.html"' + '},\n'
+lines = {}
+# assumes that your directory structure has matplotlib/ and mpl-brochure-site/
+# at the same level relative each other.
+home = Path('../../../../matplotlib/galleries')
+for fn in home.rglob('plot_types/*/*.py'):
+    _log.info('               %s', fn)
+    name = fn.stem
 
-stout +=  '];'
+    local_vars = {}
+    global_vars = {}
+    exec(fn.read_text(), global_vars, local_vars)
+    local_vars['fig'].savefig(f'{name}300.png', dpi=300)
 
-open('../images-rotate-info.js', 'w').write(stout)
+    stop = False
+    for line in open(fn):
+        if stop:
+            cap = line[:-1]
+            break
+        if line[:3] == '===':
+            stop = True
+    lines[name] = json.dumps({
+        'image': f'{name}300.png',
+        'caption': cap,
+        'link': str(fn.relative_to(home).with_suffix(".html")),
+    })
+
+with open('../images-rotate-info.js', 'w') as stout:
+    stout.write('var images_rotate = [\n')
+    for _, line in sorted(lines.items()):
+        stout.write(f'    {line},\n')
+    stout.write('];\n')
